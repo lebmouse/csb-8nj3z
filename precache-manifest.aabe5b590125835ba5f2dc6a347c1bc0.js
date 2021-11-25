710cb743b127cbe6e144e2bcc7cd180e13d1b53a
Code Sandbox <hello@codesandbox.io>
2021-11-25 04:38:28 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.aabe5b590125835ba5f2dc6a347c1bc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a462362cf2d04b7ac172db20e7db2330",
+    "url": "https://lebmouse.github.io/csb-8nj3z/index.html"
+  },
+  {
+    "revision": "b1ccc1e5c948a719908a",
+    "url": "https://lebmouse.github.io/csb-8nj3z/static/css/main.7af86b17.chunk.css"
+  },
+  {
+    "revision": "b55c90b6fbfe7a0b6e69",
+    "url": "https://lebmouse.github.io/csb-8nj3z/static/js/2.cb004c2c.chunk.js"
+  },
+  {
+    "revision": "913d4e2c9aac1c58eda0b0e0ede65b59",
+    "url": "https://lebmouse.github.io/csb-8nj3z/static/js/2.cb004c2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1ccc1e5c948a719908a",
+    "url": "https://lebmouse.github.io/csb-8nj3z/static/js/main.ce9ee7a3.chunk.js"
+  },
+  {
+    "revision": "f572985299e43345b418",
+    "url": "https://lebmouse.github.io/csb-8nj3z/static/js/runtime-main.d18faa90.js"
+  }
+]);
